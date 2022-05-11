544ddf35ed7712db1c2e455ed5a41d3962769514
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-11 16:42:56 +0200

    Initial Commit

---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by Murat Gültürk on 11.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
